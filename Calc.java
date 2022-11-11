a2c91d3d14bb109e9991f35de4363f5859aaa898
AJMAL MOHAMED YASEEN <43436828+ajmalmyaseen@users.noreply.github.com>
2022-11-11 21:29:07 +0530

    basic done

---
+++ Calc.java
@@ -0,0 +1,11 @@
+
+public class Calc {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num=10;
+		int result=num+30;
+		System.out.println(result);
+	}
+
+}
