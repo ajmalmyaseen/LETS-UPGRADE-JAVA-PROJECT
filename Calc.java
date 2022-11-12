792d51215e71ddd71a9c54b9d8595e66dd596b7c
AJMAL MOHAMED YASEEN <43436828+ajmalmyaseen@users.noreply.github.com>
2022-11-12 20:50:40 +0530

    Update Calc.java

--- Calc.java
+++ Calc.java
@@ -1,11 +1,28 @@
-
+import java.util.Scanner;
 public class Calc {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int num=10;
-		int result=num+30;
-		System.out.println(result);
+		Scanner in = new Scanner(System.in);
+
+		
+		System.out.print("Input first number: ");
+  		int num1 = in.nextInt();
+   
+  		System.out.print("Input second number: ");
+  		int num2 = in.nextInt();
+   
+ 
+  		System.out.println(num1 + " + " + num2 + " = " + 
+  		(num1 + num2));
+   
+  		System.out.println(num1 + " - " + num2 + " = " + 
+  		(num1 - num2));
+   
+ 		 System.out.println(num1 + " x " + num2 + " = " + 
+  		(num1 * num2));
+   
+  		System.out.println(num1 + " / " + num2 + " = " + 
+  		(num1 / num2));
 	}
 
 }
