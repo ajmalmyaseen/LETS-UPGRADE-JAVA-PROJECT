971a597783168b43f1241155a0cdad2273ea9fc4
AJMAL MOHAMED YASEEN <43436828+ajmalmyaseen@users.noreply.github.com>
2022-11-12 22:16:34 +0530

    made it even number check

--- palindrome.java
+++ palindrome.java
@@ -6,18 +6,21 @@ public class Palindrome{
         System.out.println("Enter number: ");
         int original=sc.nextInt();
         int temp=original;
-
-        while(temp>0){
-            int digit=temp%10;
-            temp=temp/10;
-            reverse=reverse*10+digit;
+        if (original % 2 == 0) {
+            System.out.println("Entered Number is Even");
+            while(temp>0){
+                int digit=temp%10;
+                temp=temp/10;
+                reverse=reverse*10+digit;
+             }
+            if(reverse == original){
+                System.out.println("Palindrome number");
+            }
+            else{
+                System.out.println("Not a Palindrome number");
+            }
         }
-        if(reverse==original){
-            System.out.println("Palindrome number");
-        }
-        else{
-            System.out.println("Not a Palindrome number");
-        }
-
+        else{System.out.println("Entered Number is Odd");
+        }    
         }
     } 
