19c6bcdf5ef8b9a3aec94a7510fb3325b5db3811
AJMAL MOHAMED YASEEN <43436828+ajmalmyaseen@users.noreply.github.com>
2022-11-12 23:16:17 +0530

    working but no return

--- Divide.java
+++ Divide.java
@@ -1,1 +1,10 @@
 
+public class Divide{
+    public static void main(String[] args) {
+        System.out.println("Divided by 23: ");
+        for(int i=10;i<=150;i++)
+            if (i % 23 == 0)
+            System.out.println(i++);
+    
+    }
+}
